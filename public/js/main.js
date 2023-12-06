eff84781d79956cc9ac1af27d3e0e36c1a7c58d7
mokhelek <mkatleho191@gmail.com>
2023-12-06 15:35:25 -0500

    add reciept scanner

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    const captureBtn = document.getElementById('captureBtn');
+    const resultDiv = document.getElementById('result');
+
+    // Access the camera
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+            video.srcObject = stream;
+        })
+        .catch((error) => {
+            console.error('Error accessing camera:', error);
+        });
+
+    // Capture image from video feed
+    captureBtn.addEventListener('click', () => {
+        const context = canvas.getContext('2d');
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        // Perform OCR on the captured image
+        Tesseract.recognize(
+            canvas.toDataURL('image/png'),
+            'eng',
+            { logger: info => console.log(info) }
+        ).then(({ data: { text } }) => {
+
+            resultDiv.innerHTML = `<p>Extracted Text: ${text}</p>`;
+            
+        }).catch((error) => {
+            console.error('OCR Error:', error);
+        });
+    });
+});
