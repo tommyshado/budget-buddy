a54eeed546ea8f31dc73ac004abdda97b1b3c1ea
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-12-07 02:51:13 +0200

    added more database tables, inserted data into categories table in script.sql

---
+++ sql/scripts.sql
@@ -0,0 +1,4 @@
+INSERT into categories (category_id, category_type) values ('1', 'Food');
+INSERT into categories (category_id, category_type) values ('2', 'Fashion');
+INSERT into categories (category_id, category_type) values ('3', 'Household');
+INSERT into categories (category_id, category_type) values ('4', 'Uncategorized');
--- sql/tables.sql
+++ sql/tables.sql
@@ -5,3 +5,20 @@ CREATE TABLE user_table (
     email VARCHAR(100) UNIQUE,
     password_hash VARCHAR(255) NOT NULL
 );
+
+-- table for all the available categories
+CREATE TABLE categories (
+    category_id SERIAL PRIMARY KEY,
+    category_type VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- table for all the products
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    product TEXT NOT NULL,
+    price NUMERIC,
+
+    -- foreign keys
+    category_id INT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
